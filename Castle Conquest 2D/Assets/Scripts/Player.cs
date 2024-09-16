7effabf43298f6b0078b3f3114c38b0d55114ae5
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-16 18:21:56 -0500

    Added Running logic to trigger run animation

--- Castle Conquest 2D/Assets/Scripts/Player.cs
+++ Castle Conquest 2D/Assets/Scripts/Player.cs
@@ -7,10 +7,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10.0f;
     Rigidbody2D rigidBody2D;
+    Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,13 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, rigidBody2D.velocity.y);
         FlipSprite();
         rigidBody2D.velocity = playerVelocity;
+        ChangingToRunningState();
+    }
+
+    private void ChangingToRunningState()
+    {
+        bool runningHorizontally = Mathf.Abs(rigidBody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", runningHorizontally);
     }
 
     private void FlipSprite()
