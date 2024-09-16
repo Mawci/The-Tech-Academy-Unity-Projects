7404f6c83546a64c3b276aedbaf7d0971668bd50
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-16 09:31:27 -0500

    Added movement with player input
    Made the square move when the arrow keys are pressed

--- 2DCube/Assets/SquareScript.cs
+++ 2DCube/Assets/SquareScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class SquareScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody2D;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myRigidbody2D.velocity = new Vector2(0f, 10f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            myRigidbody2D.velocity = new Vector2(0f, -10f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            myRigidbody2D.velocity = new Vector2(-10f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myRigidbody2D.velocity = new Vector2(10f, 0f);
+        }
+
     }
 }
