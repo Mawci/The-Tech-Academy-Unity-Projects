c8343d40e192dca69ffa74289640ec5ac0c35fd4
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-16 09:44:11 -0500

    Added if statement to get boundaries

--- 2DCube/Assets/SquareScript.cs
+++ 2DCube/Assets/SquareScript.cs
@@ -32,5 +32,18 @@ public class SquareScript : MonoBehaviour
             myRigidbody2D.velocity = new Vector2(10f, 0f);
         }
 
+        if(transform.position.x > 9.5f)
+        {
+            Debug.LogWarning("Our Cube is out of bounds to the right side!!");
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            Debug.LogWarning("Our Cube is out of bounds to the left side");
+        }
+        else if(transform.position.y > 5.51f)
+        {
+            Debug.LogWarning("Our cube is out of bounds in the top");
+        }
+
     }
 }
