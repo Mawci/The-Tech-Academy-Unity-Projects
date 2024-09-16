87c73227f0b1b7bf786fb4ea173696bfc9d7820b
Mawci <79284679+Mawci@users.noreply.github.com>
2024-09-16 08:46:54 -0500

    Added player input

--- 3DCube/Assets/SpinCube.cs
+++ 3DCube/Assets/SpinCube.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class SpinCube : MonoBehaviour
 {
+    int numberOfTimes = 5;
+    string nameOfKey = "ENTER";
+    float speed = 5.7f;
+
     public Vector3 RotateAmount;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Press any arrow key");
-        Debug.LogWarning("No input detected");
-        Debug.LogError("There is nothing set up to register input!");
-        Debug.Log("Adding a new line of code");
+        //Debug.Log($"Press any {numberOfTimes} arrow key");
+        //Debug.LogWarning($"No {nameOfKey} detected");
+        //Debug.LogError("There is nothing set up to register input!");
+        //Debug.Log($"Adding a new line of code with speed {speed}");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(RotateAmount);
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("The up arrow was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("The right arrow was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("The left arrow was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("The down arrow was pressed.");
+        }
     }
 }
